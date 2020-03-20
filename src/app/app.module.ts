71db383e3f6102820557518765edeb2970d4cea5
duycuong96 <vuduycuong996@gmail.com>
2020-03-20 23:03:10 +0700

    1 file modified, 16 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,19 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { CategoryComponent } from './category/category.component';
+import { FooterComponent } from './footer/footer.component';
+import { MenuComponent } from './menu/menu.component';
+import { ProductComponent } from './product/product.component';
+import { ProductAddComponent } from './product-add/product-add.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { SlideComponent } from './slide/slide.component';
+import { ProductService } from './product.service';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [ AppComponent, HelloComponent, CategoryComponent, FooterComponent, MenuComponent, ProductComponent, ProductAddComponent, ProductListComponent, SlideComponent ],
+  bootstrap:    [ AppComponent ],
+  providers: [ProductService]
 })
 export class AppModule { }
